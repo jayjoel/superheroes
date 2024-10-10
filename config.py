76df50e85685c72099ff73a53154ded5ecb75078
joel_patrick <jayjoelpn@gmail.com>
2024-10-10 15:35:10 +0300

    all other files committed

---
+++ config.py
@@ -0,0 +1,8 @@
+# import os
+
+# # config.py
+# class Config:
+#     SECRET_KEY = 'your-secret-key'
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///superpower.db'  
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False  
+
